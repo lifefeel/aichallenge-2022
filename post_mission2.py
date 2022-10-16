9aa6f01ebc8ad2c659bf6a3087a2224b25fee855
J.P Lee <koreanfeel@gmail.com>
2022-10-16 19:56:09 +0900

    제출 형식 수정, 프레임을 시간으로 변환

--- post_mission2.py
+++ post_mission2.py
@@ -10,13 +10,19 @@ def generate_answer_sheet(cam_no, mission, answer):
     return {
         'team_id': TEAM_ID,
         'secret': SECRET,
-        'answer': answer
+        'answer_sheet': {
+            'cam_no': str(cam_no),  # TODO 숫자만 들어가도록 처리 필요 (예: "03")
+            'mission': str(mission),  # TODO 숫자만 들어가도록 처리 필요 (예: "2")
+            'answer': answer
+        }
     }
 
 
 if __name__ == '__main__':
     video_result_file = 'results/cam1_short_result.json'
     audio_result_file = 'mission2_result.json'
+
+    video_fps = 15
 
     video_results = load_json(video_result_file)
     audio_results = load_json(audio_result_file)
@@ -54,6 +60,31 @@ if __name__ == '__main__':
 
         time_start = audio_start.strftime('%H:%M:%S')
         time_end = audio_end.strftime('%H:%M:%S')
+
+        # time to frame
+        start_frame = start_time * video_fps
+        end_frame = end_time * video_fps
+
+        print(f'start_frame : {start_frame}')
+        print(f'end_frame : {end_frame}')
+
+        for frame_result in frame_results:
+            assert len(frame_result.keys()) == 2
+
+            frame_number = frame_result['frame_number']
+
+            if frame_number < start_frame or frame_number > end_frame:
+                continue
+
+            print(f'video time : {frame_number / video_fps}')
+
+            results = frame_result['result']
+
+            print(f'result len : {len(results)}')
+            for result in results:
+                print(result)
+
+            # print(frame_result)
 
         answer = {
             'event': label,
